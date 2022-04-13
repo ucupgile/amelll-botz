3029e90b9584a8087731f03f2b272e054293c5bc
D'anang.W.S™✓ <98865918+Danangws@users.noreply.github.com>
2022-04-13 22:17:16 +0700

    Update belibot.js

--- plugins/belibot.js
+++ plugins/belibot.js
@@ -27,8 +27,8 @@ let handler = async (m, { conn }) => {
 *[❗] Jika Group Sepi 2 minggu, bot akan leave*
 
 ◪ *Jika Minat Hubungi owner kami*
-◪ *Atau Masuk Group Official Bot*
-_https://chat.whatsapp.com/Hjr3H7l9iC8F2deP0cv6GT_
+◪ *_wa.me/6285715590172_*
+
 
 ▌│█║▌║▌║║▌║▌║█│▌
 `.trim(), `${wm}`, 'Payment', '#payment', 'Owner', '#Nowner', m)
